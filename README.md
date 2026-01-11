d23d30f9ad98f9ceaef64ef0244e58c8173cc8ef
Safarveisi <sajad.safarveisi@gmail.com>
2026-01-11 18:52:03 +0100

    Application deployment using ECS Fargate

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ## Connecting an EC2 instance to S3
 
-![architecture](./connect_ec2_to_s3/assets/ec2_to_s3.png)
+![architecture](./assets/ec2_to_s3.png)
+
+## Deploying your application using ECS Fargate and ALB
+
+![architecture](./assets/ec2_to_s3.png)
---
+++ ecs_and_alb/main.tf
@@ -0,0 +1,204 @@
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = "sts:AssumeRole"
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "${var.base_cidr}"
+  tags = {
+    Name = "Project VPC"
+  }
+}
+
+resource "aws_subnet" "public_subnets" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.public_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+  tags = {
+    Name = "Public Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Project VPC IGW"
+  }
+}
+
+resource "aws_route_table" "second_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example.id
+  }
+  tags = {
+    Name = "2nd Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.second_rt.id
+}
+
+resource "aws_security_group" "alb_security_group" {
+  name = "allow_traffic"
+  description = "Allow inbound traffic to ALB on port 80"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress  {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_security_group" "ecs_security_group" {
+  name = "allow_ecs_traffic"
+  description = "Allow inbound traffic to ECS containers from ALB"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    description = "HTTP from ALB"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    security_groups = [aws_security_group.alb_security_group.id]
+  }
+  egress  {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_lb" "load_balancer" {
+  name               = "example-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_security_group.id]
+  subnets            = [aws_subnet.public_subnets[0].id, aws_subnet.public_subnets[1].id]
+  tags = {
+    Name = "Project ALB"
+  }
+}
+
+
+resource "aws_lb_listener" "lb_listener" {
+  load_balancer_arn  = aws_lb.load_balancer.arn
+  port               = 80
+  protocol           = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+  }
+}
+
+resource "aws_lb_target_group" "alb_target_group" {
+  name        = "example-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+
+  health_check {
+    path  = "/"
+    interval = 20
+  }
+
+  tags = {
+    Name = "Project ALB Target Group"
+  }
+}
+
+resource "aws_ecs_cluster" "cs_cluster" {
+  name = "example-cluster"
+}
+
+resource "aws_ecs_cluster_capacity_providers" "ecs_providers" {
+  cluster_name       = aws_ecs_cluster.cs_cluster.name
+  capacity_providers = ["FARGATE"]
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE"
+    base              = 1
+    weight            = 100
+  }
+}
+
+resource "aws_ecs_task_definition" "ecs_task_definition" {
+  family                   = "example-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  cpu                      = "256"
+  memory                   = "512"
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture = "X86_64"
+  }
+  container_definitions = jsonencode([
+    {
+      name      = "example-container"
+      image     = "public.ecr.aws/nginx/nginx:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "ecs_service" {
+  name            = "example-service"
+  cluster         = aws_ecs_cluster.cs_cluster.id
+  task_definition = aws_ecs_task_definition.ecs_task_definition.arn
+  desired_count   = 1
+  network_configuration {
+    subnets         = [aws_subnet.public_subnets[0].id, aws_subnet.public_subnets[1].id]
+    assign_public_ip = true
+    security_groups = [aws_security_group.ecs_security_group.id]
+  }
+  launch_type = "FARGATE"
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+    container_name   = "example-container"
+    container_port   = 80
+  }
+
+}
